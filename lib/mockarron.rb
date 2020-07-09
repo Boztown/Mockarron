d9bbd51bc4525d2efe2cfeed3b15755416fae722
Ryan Bosinger <rbosinger@appcolony.ca>
2020-07-08 23:38:31 -0700

    Add bootable Sinatra server

--- lib/mockarron.rb
+++ lib/mockarron.rb
@@ -4,7 +4,14 @@ require 'sinatra'
 module Mockarron
   class Error < StandardError; end
 
+  class WebServer < Sinatra::Base
+    get '/' do
+      "I started this gangsta shit."
+    end
+  end
+
   def self.run
     puts "something."
   end
 end
+
