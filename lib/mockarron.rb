2d43f5759d2cb0824bbe1cea429e0006c39afa08
Ryan Bosinger <rbosinger@appcolony.ca>
2020-07-08 23:56:32 -0700

    Add that funky live reloading

--- lib/mockarron.rb
+++ lib/mockarron.rb
@@ -1,16 +1,20 @@
 require "mockarron/version"
 require 'sinatra'
+require 'sinatra/reloader'
 require 'yaml'
 
 module Mockarron
   class Error < StandardError; end
 
   class WebServer < Sinatra::Base
+    configure :development do
+      register Sinatra::Reloader
+    end
     # $route_defs = YAML.load(File.read("routes.yaml"))
     # $routes = $route_defs.map { |r| Route.new(r) }
 
     get '/' do
-      "UI for route selection upcoming."
+      "UI for route selection upcoming?"
     end
   end
 end
