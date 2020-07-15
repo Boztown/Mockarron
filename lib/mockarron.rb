0c7b94f641260152c1e25b0c9a49b9af326dc273
Ryan Bosinger <rbosinger@appcolony.ca>
2020-07-15 12:20:11 -0700

    Deal with YAML loading error, add specs

--- lib/mockarron.rb
+++ lib/mockarron.rb
@@ -1,20 +1,38 @@
 require 'pry-byebug'
 require "mockarron/version"
-require 'sinatra'
-require 'sinatra/reloader'if settings.environment == :development
+require 'sinatra/base'
+# require 'sinatra/reloader'if settings.environment == :development
 require 'yaml'
 
 module Mockarron
   class Error < StandardError; end
 
-  class WebServer < Sinatra::Base
-    configure :development do
-      # register Sinatra::Reloader
+  class App
+    ROUTE_DATA_FILE = "routes.yaml"
+
+    def thing
+      binding.pry
     end
-    # $route_defs = YAML.load(File.read("routes.yaml"))
-    # $routes = $route_defs.map { |r| Route.new(r) }
+
+    def load_route_data
+      route_data = YAML.load(File.read(ROUTE_DATA_FILE))
+      route_data.map { |r| "a" }
+
+    rescue Errno::ENOENT => e
+      raise Mockarron::Error.new("Cannot open file: #{ROUTE_DATA_FILE}")
+    end
+  end
+
+  class WebServer < Sinatra::Base
+    configure do
+      app = Mockarron::App.new
+      $routes = app.load_route_data
+    rescue Mockarron::Error => e
+      $routes = []
+    end
 
     get '/' do
+      @routes = $routes
       erb :index
     end
   end
