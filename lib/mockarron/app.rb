9d05a3d4ebfdda40cd7d1added2177a59acc6227
Ryan Bosinger <rbosinger@appcolony.ca>
2020-07-15 17:14:52 -0700

    Spike: "mockarron new" (new project)

--- lib/mockarron/app.rb
+++ lib/mockarron/app.rb
@@ -4,9 +4,19 @@ module Mockarron
   class App
     ROUTE_DATA_FILE = "routes.yaml"
 
+    def new_project
+      unless File.exists? ROUTE_DATA_FILE
+        File.write("routes.yaml", "Hi")
+      end
+    end
+
     def load_route_data
-      route_data = YAML.load(File.read(ROUTE_DATA_FILE))
-      route_data.map { |r| Route.new(r) }
+      if File.exists? ROUTE_DATA_FILE
+        route_data = YAML.load_file(ROUTE_DATA_FILE)
+        route_data.map { |r| Route.new(r) }
+      else
+        false
+      end
 
     rescue Errno::ENOENT
       raise Mockarron::Error.new("Cannot open file: #{ROUTE_DATA_FILE}")
--- lib/mockarron/cli.rb
+++ lib/mockarron/cli.rb
@@ -1,8 +1,15 @@
 require 'thor'
 require 'mockarron/web_server'
+require 'mockarron/app'
 
 module Mockarron
   class CLI < Thor
+    desc "new", "Creates a new Mockarron project"
+    def new
+      mockarron = Mockarron::App.new
+      mockarron.load_route_data
+    end
+
     desc "server", "Starts the Mockarron web server"
     def server
       puts "Starting Mockarron..."
