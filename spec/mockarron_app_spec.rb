7152fbd963c0c2b2e3525f5d00d8fd68686b731a
Ryan Bosinger <rbosinger@appcolony.ca>
2020-08-11 23:29:13 -0700

    Add spec for YAML loading and fallback

--- spec/mockarron_app_spec.rb
+++ spec/mockarron_app_spec.rb
@@ -1,17 +1,38 @@
 require 'spec_helper'
 
 RSpec.describe Mockarron::App do
+  describe "#new_project" do
+    xcontext "when the selected project path is not empty" do
+    end
+  end
+
   describe "#load_route_data" do
     context "when the routes YAML file isn't where it should be" do
       before { stub_const("Mockarron::App::ROUTE_DATA_FILE", "some_file_that_doesnt_exist.yaml") }
 
-      it "returns false" do
-        expect(subject.load_route_data).to be false
+      it "falls back to loading routes.template.yaml" do
+        expect(YAML).to receive(:load_file)
+          .with(Mockarron::App::ROUTE_TEMPLATE_FILE)
+          .and_call_original
+
+        subject.load_route_data
       end
     end
 
-    context "when" do
+    context "when the routes YAML file is available" do
+      # `routes.yaml` doesn't exist, so we'll point it to the template file
+      before { stub_const("Mockarron::App::ROUTE_DATA_FILE", "routes.template.yaml") }
 
+      it "loads up and parses the file" do
+        expect(YAML).to receive(:load_file)
+          .with(Mockarron::App::ROUTE_DATA_FILE)
+          .and_call_original
+
+        routes = subject.load_route_data
+        expect(routes).to be_a Array
+        expect(routes.first).to be_a Mockarron::Route
+        expect(routes.first.responses.first).to be_a Mockarron::RouteResponse
+      end
     end
   end
 end
